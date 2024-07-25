b27c3fedddb8e265e4b5c7a5daf5265405130ccc
Sunny Prakash <sunny.pprakash@gmail.com>
2024-07-25 15:15:09 +0530

    auth and login

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -29,7 +29,7 @@ export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
     <form action={deleteInvoiceWithId}>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
+      <button className="rounded-md border p-2 hover:bg-red-200">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
